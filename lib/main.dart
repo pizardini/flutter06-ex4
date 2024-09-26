5674b7d8525997ff6a5da98a38200f21d30dba92
Pietro Zardini <pizardini@gmail.com>
2024-09-26 20:49:44 -0300

    implementação do geolocator

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,20 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter06/local.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:http/http.dart' as http;
 
-void main() {
-  runApp(const MaterialApp(
-    home: MainApp(),
+void main() async {
+WidgetsFlutterBinding.ensureInitialized();
+Position position = await _determinePosition();
+  runApp(MaterialApp(
+    home: MainApp(initialPosition: position),
   ));
 }
 
 class MainApp extends StatefulWidget {
-  const MainApp({super.key});
+  final Position initialPosition;
+  const MainApp({super.key, required this.initialPosition});
 
   @override
   State<MainApp> createState() => _MainAppState();
@@ -22,6 +26,15 @@ class _MainAppState extends State<MainApp> {
   final TextEditingController _longController = TextEditingController();
   var temperatura = '';
   var umidade = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _latController.text = widget.initialPosition.latitude.toString();
+    _longController.text = widget.initialPosition.longitude.toString();
+
+    print(widget.initialPosition);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -52,6 +65,7 @@ class _MainAppState extends State<MainApp> {
   }
 
 void buscaTemp() async {
+
   String latUser = _latController.text;
   String longUser = _longController.text;
   String url = 'https://api.open-meteo.com/v1/forecast?latitude=$latUser&longitude=$longUser&current=temperature_2m,relative_humidity_2m&timezone=America%2FSao_Paulo&forecast_days=1';
@@ -97,4 +111,33 @@ void _showAlertDialog(String title, String content) {
     },
   );
 }
+}
+
+Future<Position> _determinePosition() async {
+  bool serviceEnabled;
+  LocationPermission permission;
+  serviceEnabled = await Geolocator.isLocationServiceEnabled();
+  if (!serviceEnabled) {
+    return Future.error('Location services are disabled.');
+  }
+
+  permission = await Geolocator.checkPermission();
+  if (permission == LocationPermission.denied) {
+    permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) {
+      return Future.error('Location permissions are denied');
+    }
+  }
+  
+  if (permission == LocationPermission.deniedForever) {
+    return Future.error(
+      'Location permissions are permanently denied, we cannot request permissions.');
+  }
+
+  final LocationSettings locationSettings = LocationSettings(
+  accuracy: LocationAccuracy.low,
+  distanceFilter: 100,
+);
+
+  return await Geolocator.getCurrentPosition(locationSettings: locationSettings);
 }
